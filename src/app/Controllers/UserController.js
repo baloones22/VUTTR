63fca20f59778e8ad093c2e8e559ccc842a004db
joaomenna1 <joaomenna1@gmail.com>
2020-02-11 19:10:29 -0400

    feature: controller for created users

---
+++ src/app/Controllers/UserController.js
@@ -0,0 +1,28 @@
+const Yup = require('yup');
+const User = require('../models/User');
+
+class UserController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().required().min(6),
+    });
+    if(!( await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "validation fails from yup" })
+    }
+
+    const userExist = await User.findOne({ where: { email: req.body.email }});
+
+    if(userExist) {
+      return res.status(400).json({ error: "User already exists."});
+    }
+
+    const user = await User.create(req.body);
+
+    return res.json(user);
+
+  }
+}
+
+module.exports = new UserController();
