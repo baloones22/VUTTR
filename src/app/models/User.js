8f09291812f3857f0aa734c6f9a11501b583e9d7
joaomenna1 <joaomenna1@gmail.com>
2020-02-11 19:10:59 -0400

    feature: models de users

---
+++ src/app/models/User.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+const { Model } = require('sequelize')
+
+class User extends Model {
+  static init(sequelize) {
+    super.init({
+      name: Sequelize.STRING,
+      email: Sequelize.STRING,
+      password_hash: Sequelize.STRING,
+    },
+      {
+        sequelize,
+      }
+    );
+      return this;
+  }
+}
+
+module.exports = User;
