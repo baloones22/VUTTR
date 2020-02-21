b03fdd28dbead29b2457d278350dd201f9713fe2
baloones22 <51084124+baloones22@users.noreply.github.com>
2020-02-21 10:57:08 -0400

    Add files via upload

--- src/database/index.js
+++ src/database/index.js
@@ -1,24 +1,18 @@
 const Sequelize = require('sequelize')
-
-const User  = require('../app/models/User')
-
 const dataBaseConfig = require('../config/database')
-
+//const User  = require('../app/models/User')
+const Tools = require('../app/models/Tools')
 const models = [
-  User,
+  //User,
+  Tools,
 ];
-
 class Database {
   constructor() {
     this.init();
   }
-
   init() {
     this.connection = new Sequelize(dataBaseConfig);
-
     models.map(model => model.init(this.connection));
-
   }
 }
-
 module.exports = new Database();
